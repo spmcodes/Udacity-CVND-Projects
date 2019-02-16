24d021aedd51bb47369777941ef3e98a2bb6d1dd
Santhosh P Mathew <43490900+spmcodes@users.noreply.github.com>
2019-02-16 15:48:14 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,93 @@
+﻿## TODO: define the convolutional neural network architecture
+
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+# can use the below import should you choose to initialize the weights of your Net
+import torch.nn.init as I
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        
+        ## TODO: Define all the layers of this CNN, the only requirements are:
+        ## 1. This network takes in a square (same width and height), grayscale image as input
+        ## 2. It ends with a linear layer that represents the keypoints
+        ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
+        
+        # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
+        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
+        
+        #Convolutional Layers - parameters - (input channels, output channels, kernel size)
+        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv2 = nn.Conv2d(32, 64, 3)
+        self.conv3 = nn.Conv2d(64, 128, 3)
+        self.conv4 = nn.Conv2d(128, 256, 2)
+        
+        ## Note that among the layers to add, consider including:
+        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        
+        #Maxpooling Layers - parameters - (kernel_size, stride)
+        self.pool = nn.MaxPool2d(2, 2)
+        
+        #Fully Coonected Layers - parameters - (input features, output features)
+        self.fc1 = nn.Linear(36864, 1000)
+        self.fc2 = nn.Linear(1000, 1000)
+        self.fc3 = nn.Linear(1000, 136)
+        
+        #Dropouts
+        self.drop1 = nn.Dropout(p = 0.1)
+        self.drop2 = nn.Dropout(p = 0.2)
+        self.drop3 = nn.Dropout(p = 0.3)
+        self.drop4 = nn.Dropout(p = 0.4)
+        self.drop5 = nn.Dropout(p = 0.5)
+        self.drop6 = nn.Dropout(p = 0.6)
+
+        
+             
+    def forward(self, x):
+        ## TODO: Define the feedforward behavior of this model
+        ## x is the input image and, as an example, here you may choose to include a pool/conv step:
+        ## x = self.pool(F.relu(self.conv1(x)))
+        
+        # First - Convolution + Activation + Pooling + Dropout
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.pool(x)
+        x = self.drop1(x)
+        #print("First size: ", x.shape)
+        
+        # Second - Convolution + Activation + Pooling + Dropout
+        x = self.drop2(self.pool(F.relu(self.conv2(x))))
+        #print("Second size: ", x.shape)
+        
+        # Third - Convolution + Activation + Pooling + Dropout
+        x = self.drop3(self.pool(F.relu(self.conv3(x))))
+        #print("Third size: ", x.shape)
+        
+        # Forth - Convolution + Activation + Pooling + Dropout
+        x = self.drop4(self.pool(F.relu(self.conv4(x))))
+        #print("Forth size: ", x.shape)
+        
+        # Flattening the layer
+        x = x.view(x.size(0), -1)
+
+        #print("Flatten size: ", x.shape)
+        
+        # First - Dense + Activation + Dropout
+        x = self.drop5(F.relu(self.fc1(x)))
+        #print("First dense size: ", x.shape)
+        
+        # Second - Dense + Activation + Dropout
+        x = self.drop6(F.relu(self.fc2(x)))
+        #print("Second dense size: ", x.shape)
+        
+        # Final Dense Layer
+        x = self.fc3(x)
+        #print("Final dense size: ", x.shape)
+        
+        # a modified x, having gone through all the layers of your model, should be returned
+        return x
